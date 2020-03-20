23606d0d41f009e890d85d7bf8086f21632724c5
Sourabh S Nath <sourabhsnath@gmail.com>
2020-03-20 08:56:07 +0530

    Added  solution for FindDuplicateWords

---
+++ src/solutions/java/random/FindDuplicateWords.java
@@ -0,0 +1,29 @@
+package random;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class FindDuplicateWords{
+
+    public static void main(String[] args){
+        List<String> strings = new ArrayList<>();
+        strings.add("A string with a duplicate");
+        strings.add("This string doesn't hava a duplicate");
+        strings.add("Row, row, row your boat");
+        strings.add("Here is a here is a here");
+        strings.add("Meow, meow. meeeoew meow, meow");
+
+
+        for(String s : strings){
+            var stringArray = s.split(" ");
+
+            var map = new HashMap<String, Integer>();
+            for(String word : stringArray){
+                map.merge(word.toLowerCase().replaceAll("[^a-zA-Z]", ""), 1, Integer::sum);
+            }
+            
+            System.out.println(map);
+        }
+    }
+}
