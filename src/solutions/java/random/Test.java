32143b1e30c8599bb027a36abc4d65d8e24fcba8
Sourabh S Nath <sourabhsnath@gmail.com>
2020-03-20 18:52:53 +0530

    Updated gitignore

--- src/solutions/java/random/Test.java
+++ src/solutions/java/random/Test.java
@@ -4,28 +4,7 @@ import java.util.*;
 
 public class Test {
     public static void main(String[] args) {
-
-        Scanner scanner = new Scanner(System.in);
-
-        var sCount = scanner.nextInt();
-        var students = new ArrayList<Integer>();
-
-
-        for (int i = 0; i < sCount; i++) {
-            students.add(scanner.nextInt());
-        }
-
-
-        var maxValue = Integer.MIN_VALUE;
-        for (int i = 0; i < sCount; i++) {
-
-            var currentValue = students.get(i);
-            if (maxValue <= currentValue) {
-                maxValue = currentValue;
-                System.out.println(0);
-            }else {
-                System.out.println(i);
-            }
-        }
+        
+        var map = new HashMap<String, Integer>();
     }
 }
